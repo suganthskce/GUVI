5aea8c2aefa1e3bc2fd32223fe8e0801642ed3da
suganthskce <suganth2610@gmail.com>
2017-06-09 10:09:09 +0530

    Create VowerOrConsonant.py

--- Beginner/Set 1/VowerOrConsonant.py
+++ Beginner/Set 1/VowerOrConsonant.py
@@ -1,7 +1,9 @@
 def function(c):
 	if c=='a' or c=='A' or c=='e' or c=='E' or c=='i' or c=='I' or c=='o' or c=='O' or c=='u' or c=='U':
 		return "Vowel"
-	else:
+	elif c>='A' and c<='Z' or c>='a' and c<='z':
 		return "Consonant"
+	else:
+		return 0
 n = raw_input()
 print(function(n))
